79491dca09df364c0510c3d5caec254b204b1210
Artem Nazarov <krongr.study@ya.ru>
2022-03-22 20:57:32 +0300

    Initial commit

---
+++ tests/test_unit_test.py
@@ -0,0 +1,10 @@
+import unittest
+from app import (check_document_existance, get_doc_owner_name,
+    get_all_doc_owners_names, remove_doc_from_shelf,
+    add_new_shelf, append_doc_to_shelf, delete_doc,
+    get_doc_shelf, move_doc_to_shelf, show_document_info,
+    show_all_docs_info, add_new_doc)
+
+
+class TestFunctions(unittest.TestCase):
+    pass
